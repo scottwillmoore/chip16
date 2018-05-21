96f5a926b300c192e9ccf02d9a108990dc187864
Scott Moore <scott.will.moore@gmail.com>
2018-05-21 23:36:44 +1000

    add failure crate and organise lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,26 @@
 extern crate byteorder;
+
 extern crate crc;
+
+#[macro_use]
+extern crate failure;
+
 extern crate rand;
 
 mod cpu;
-mod flags;
-mod instruction;
-mod memory;
-mod registers;
-mod rom;
-
 pub use cpu::Cpu;
+
+mod flags;
 pub use flags::Flags;
+
+mod instruction;
 pub use instruction::{Condition, Instruction};
+
+mod memory;
 pub use memory::{Memory, Read, Write};
+
+mod registers;
 pub use registers::Registers;
+
+mod rom;
 pub use rom::Rom;
